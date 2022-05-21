16a4006de8518bd3e3e8b6a2bd02862a30564d0d
fogleins <fogleins@hotmail.hu>
2022-05-21 22:09:07 +0200

    Update Lens model

--- lib/data/domain_model/lens.dart
+++ lib/data/domain_model/lens.dart
@@ -1,10 +1,19 @@
 import 'package:my_photo_gear/data/domain_model/photo_gear_base.dart';
 
 class Lens extends PhotoGear {
+  final double maximumAperture;
+  final double minimumAperture;
   final int filterThreadDiameter;
   final bool hasImageStabilization;
 
-  Lens(String make, String model, String serialNumber, String note,
-      this.filterThreadDiameter, this.hasImageStabilization)
+  Lens(
+      String make,
+      String model,
+      String serialNumber,
+      String note,
+      this.maximumAperture,
+      this.minimumAperture,
+      this.filterThreadDiameter,
+      this.hasImageStabilization)
       : super(make, model, serialNumber, note);
 }
